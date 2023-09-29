2024aa9ff9aea4445453d0ed70cdba89d06dced6
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-09-29 10:13:37 +0600

    Add login using email and password

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,68 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import auth from "../../firebase/firebase.config";
 
 const Login = () => {
+
+    const [loginError, setLoginError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
+
+
+    const handleLogin = e => {
+        e.preventDefault()
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+                setSuccess("Login successful")
+            })
+            .catch(error => {
+                setLoginError(error.message)
+                console.log(error.message)
+            })
+    }
+
+
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+
     return (
         <div>
-            <h2 className="text-3xl">Please Login</h2>            
+            <div className="mx-auto w-1/2">
+                <h2 className="text-3xl mb-10">Login</h2>
+                <form onSubmit={handleLogin}>
+                    <input className="w-full mb-4 px-4 py-2" required type="email" name="email" placeholder="Email" />
+                    <br />
+                    <div className="relative border">
+                        <input
+                            className="w-full px-4 py-2 mr-2"
+                            required
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            placeholder="Password" />
+                        <span onClick={handleShowPassword} className="absolute top-3 right-2">
+                            {
+                                showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                            }
+                        </span>
+                    </div>
+                    <p className="my-2">Forgot password?</p>
+
+                    <input className="btn btn-secondary w-full mb-4  px-4 py-2" type="submit" name="Login" />
+                </form>
+                {
+                    loginError && <p className="text-red-500">{loginError}</p>
+                }
+                {
+                    success && <p className="text-green-500">{success}</p>
+                }
+            </div>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,7 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
 
@@ -12,21 +13,25 @@ const Register = () => {
         e.preventDefault()
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        const accepted = e.target.terms.checked;
+        console.log(email, password, accepted);
 
-        
+
         //reset success
         setSuccess('')
-        
+
         //reset error message
         setRegisterError('');
-        
+
         // client side password check
-        if(password.length < 6){
+        if (password.length < 6) {
             setRegisterError("Password length should be at least 6 characters or longer")
             return;
-        }else if(!/[A-Z]/.test(password)){
+        } else if (!/[A-Z]/.test(password)) {
             setRegisterError("Your password should have an upper case letter")
+            return;
+        }else if(!accepted){
+            setRegisterError('Please accept our terms and conditions')
             return;
         }
 
@@ -42,26 +47,36 @@ const Register = () => {
             })
     }
 
-    const handleShowPassword = () =>{
+    const handleShowPassword = () => {
         setShowPassword(!showPassword)
-    } 
+    }
 
     return (
         <div>
             <div className="mx-auto w-1/2">
                 <h2 className="text-3xl mb-10">Please Register</h2>
                 <form onSubmit={handleRegister}>
-                    <input className="w-3/4 mb-4 px-4 py-2" required type="email" name="email" placeholder="Email" />
+                    <input className="w-full mb-4 px-4 py-2" required type="email" name="email" placeholder="Email" />
                     <br />
-                    <input 
-                    className="w-3/4 mb-4  px-4 py-2 mr-2" 
-                    required 
-                    type={ showPassword ? "text" : "password"} 
-                    name="password" 
-                    placeholder="Password" />
-                    <span onClick={handleShowPassword} className="btn btn-neutral">show password</span>
+                    <div className="relative border">
+                        <input
+                            className="w-full px-4 py-2 mr-2"
+                            required
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            placeholder="Password" />
+                        <span onClick={handleShowPassword} className="absolute top-3 right-2">
+                            {
+                                showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                            }
+                        </span>
+                    </div>
                     <br />
-                    <input className="btn btn-secondary w-3/4 mb-4  px-4 py-2" type="submit" name="Register" />
+                    <div className="mb-4">
+                        <input type="checkbox" name="terms" id="terms" />
+                        <label className="ml-2" htmlFor="terms">Please accept our <a href="#">terms and conditions</a> </label>
+                    </div>
+                    <input className="btn btn-secondary w-full mb-4  px-4 py-2" type="submit" name="Register" />
                 </form>
                 {
                     registerError && <p className="text-red-500">{registerError}</p>
