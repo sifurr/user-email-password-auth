3f8f54ca0c489dd56f0e5b21747156a84d6eee21
Md Saifur Rahman <saifurrshishir@gmail.com>
2023-09-29 07:43:47 +0600

    Add show password option

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,32 +5,70 @@ import { useState } from "react";
 const Register = () => {
 
     const [registerError, setRegisterError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
 
-    const handleRegister = e =>{
+    const handleRegister = e => {
         e.preventDefault()
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
+
+        
+        //reset success
+        setSuccess('')
+        
+        //reset error message
+        setRegisterError('');
+        
+        // client side password check
+        if(password.length < 6){
+            setRegisterError("Password length should be at least 6 characters or longer")
+            return;
+        }else if(!/[A-Z]/.test(password)){
+            setRegisterError("Your password should have an upper case letter")
+            return;
+        }
+
+        // user creation
         createUserWithEmailAndPassword(auth, email, password)
-            .then(result => console.log(result.user))
-            .catch(error => 
-            {
+            .then(result => {
+                console.log(result.user)
+                setSuccess('User created successfully')
+            })
+            .catch(error => {
                 console.log(error.message)
-                setRegisterError(error.message)
+                setRegisterError("User email already exists")
             })
     }
+
+    const handleShowPassword = () =>{
+        setShowPassword(!showPassword)
+    } 
+
     return (
         <div>
             <div className="mx-auto w-1/2">
                 <h2 className="text-3xl mb-10">Please Register</h2>
                 <form onSubmit={handleRegister}>
-                    <input className="w-3/4 mb-4 px-4 py-2" type="email" name="email" placeholder="Email" />
-                    <br />                   
-                    <input className="w-3/4 mb-4  px-4 py-2" type="password" name="password" placeholder="Password" />
-                    <br />                  
+                    <input className="w-3/4 mb-4 px-4 py-2" required type="email" name="email" placeholder="Email" />
+                    <br />
+                    <input 
+                    className="w-3/4 mb-4  px-4 py-2 mr-2" 
+                    required 
+                    type={ showPassword ? "text" : "password"} 
+                    name="password" 
+                    placeholder="Password" />
+                    <span onClick={handleShowPassword} className="btn btn-neutral">show password</span>
+                    <br />
                     <input className="btn btn-secondary w-3/4 mb-4  px-4 py-2" type="submit" name="Register" />
                 </form>
-                <p className="text-red-500">{registerError}</p>
+                {
+                    registerError && <p className="text-red-500">{registerError}</p>
+                }
+                {
+                    success && <p className="text-green-500">{success}</p>
+                }
             </div>
         </div>
     );
